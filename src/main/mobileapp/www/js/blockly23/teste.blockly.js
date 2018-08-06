f6f4ea726bd1d90232a36cc064b409073c5cd44d
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-08-06 14:36:42 +0000

    teste

---
+++ src/main/mobileapp/www/js/blockly23/teste.blockly.js
@@ -0,0 +1,11 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Teste = window.blockly.js.blockly.Teste || {};
+
+/**
+ * Bloco
+ */
+window.blockly.js.blockly.Teste.Executar = function() {
+	this.cronapi.screen.changeValueOfField("", '');
+}
